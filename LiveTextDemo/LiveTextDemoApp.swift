fe84d17dc095c131ae73c2f210f477bcf93e0c56
itsuki <itsuki.enjyoy@gmail.com>
2025-10-05 08:46:42 +0900

    Initial Commit

---
+++ LiveTextDemo/LiveTextDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTextDemoApp.swift
+//  LiveTextDemo
+//
+//  Created by Itsuki on 2025/10/05.
+//
+
+import SwiftUI
+
+@main
+struct LiveTextDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
